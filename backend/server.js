7bc28472b5b3e90db8a798ef5178883c69ce1fae
knacker65 <67683942+knacker65@users.noreply.github.com>
2023-06-18 00:42:45 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,25 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+require('dotenv').config();
+
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
-const usersRouter = require('./routes/users');
-const projectsRouter = require('./routes/projects');
+// Your routes go here
 
-app.use('/users', usersRouter);
-app.use('/projects', projectsRouter);
-
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
 });
