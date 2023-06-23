b5ef581eb1c37ad10129ca6b1d2601c3f06d0d1f
knacker65 <67683942+knacker65@users.noreply.github.com>
2023-06-23 19:54:13 +0200

    Update projects.js

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -1,18 +1,18 @@
-router.route('/add').post((req, res) => {
-  console.log(req.body);  // Add this line
-  const username = req.body.username;
-  const description = req.body.description;
-  const duration = Number(req.body.duration);
-  const date = Date.parse(req.body.date);
+const router = require('express').Router();
+let Project = require('../models/project.model');
 
-  const newProject = new Project({
-    username,
-    description,
-    duration,
-    date,
-  });
+router.route('/').get((req, res) => {
+  Project.find()
+    .then(projects => res.json(projects))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+  const newProject = new Project(req.body);
 
   newProject.save()
     .then(() => res.json('Project added!'))
     .catch(err => res.status(400).json('Error: ' + err));
 });
+
+module.exports = router;
