d50e5e45888bc55cbc830642bf3f9cd468e495f2
knacker65 <67683942+knacker65@users.noreply.github.com>
2023-06-25 19:31:31 +0200

    Update store.js

--- client/src/pages/store.js
+++ client/src/pages/store.js
@@ -1,6 +1,12 @@
-import { createStore } from 'redux';
-import rootReducer from './reducers';
+// src/pages/store.js
 
-const store = createStore(rootReducer);
+import { configureStore } from '@reduxjs/toolkit';
+import jobReducer from '../reducers/jobReducer';
+
+const store = configureStore({
+  reducer: {
+    jobs: jobReducer,
+  },
+});
 
 export default store;
