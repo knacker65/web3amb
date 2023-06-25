18cb71dbefa443959b43335ffa0addec158acfa8
knacker65 <67683942+knacker65@users.noreply.github.com>
2023-06-25 19:28:54 +0200

    Update BrowseJobs.js

--- client/src/components/BrowseJobs.js
+++ client/src/components/BrowseJobs.js
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import { useSelector } from 'react-redux';
 
 const BrowseJobs = () => {
-  const [jobs, setJobs] = useState([]);
+  const jobsFromRedux = useSelector(state => state.jobs);
   const [searchTerm, setSearchTerm] = useState('');
+  const [jobs, setJobs] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/jobs')
+    axios.get('http://localhost:5000/jobPostings')
       .then(response => {
         setJobs(response.data);
       })
@@ -18,8 +20,9 @@ const BrowseJobs = () => {
   }, []);
 
   const filteredJobs = jobs.filter(job =>
-    job.title.toLowerCase().includes(searchTerm.toLowerCase())
+    job.title && job.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
+  
 
   return (
     <div className="min-h-screen flex flex-col">
@@ -50,22 +53,26 @@ const BrowseJobs = () => {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {filteredJobs.map((job) => (
-            <div
-              key={job.id}
-              className="border border-gray-200 p-4 rounded bg-white shadow"
-            >
-              <h3 className="text-xl font-bold mb-2">{job.title}</h3>
-              <p className="text-gray-500 mb-2">{job.company}</p>
-              <p className="text-gray-500 mb-2">{job.location}</p>
-              <p className="text-gray-500 mb-2">{job.type}</p>
-              <p className="text-gray-500 mb-2">{job.description}</p>
-              <button className="bg-blue-500 text-white px-4 py-2 mt-4 rounded">
-                Apply Now
-              </button>
-            </div>
-          ))}
-        </div>
+  {jobs.map((job) => (
+    <div
+      key={job._id}
+      className="border border-gray-200 p-4 rounded bg-white shadow"
+    >
+      <h3 className="text-xl font-bold mb-2">{job.jobTitle}</h3>
+      <p className="text-gray-500 mb-2">Company: {job.companyName}</p>
+      <p className="text-gray-500 mb-2">Location: {job.jobLocation}</p>
+      <p className="text-gray-500 mb-2">Type: {job.jobType}</p>
+      <p className="text-gray-500 mb-2">Description: {job.jobDescription}</p>
+      <button className="bg-blue-500 text-white px-4 py-2 mt-4 rounded">
+        Apply Now
+      </button>
+      <button className="bg-green-500 text-white px-4 py-2 mt-4 rounded ml-4">
+        View Details
+      </button>
+    </div>
+  ))}
+</div>
+
       </div>
       <Footer />
     </div>
