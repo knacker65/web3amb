c2fa99212cf93b6119abcd10c62ffe8dbbcd75e2
knacker65 <67683942+knacker65@users.noreply.github.com>
2023-06-25 12:58:42 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Middleware to log the request body
+app.use((req, res, next) => {
+  console.log('Incoming request:', req.method, req.path, req.body);
+  next();
+});
+
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -20,9 +26,16 @@ connection.once('open', () => {
 
 const usersRouter = require('./routes/users');
 const projectsRouter = require('./routes/projects');
+const jobPostingsRouter = require('./routes/jobPostings'); // Add this line
 
 app.use('/users', usersRouter);
 app.use('/projects', projectsRouter);
+app.use('/jobPostings', jobPostingsRouter); // Add this line
+
+// Error handling for unknown routes
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Route not found' });
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
