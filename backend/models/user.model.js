96a4f0f25ca64404de17f4502927967399953434
knacker65 <67683942+knacker65@users.noreply.github.com>
2023-06-27 10:56:14 +0200

    Update user.model.js

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,22 +3,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: { type: String, required: true },
+  fullName: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: String, required: true },
-  country: { type: String, required: true },
-  language: { type: String, required: true },
-  timeCommitment: { type: String, required: true },
-  motivation: { type: String, required: true },
-  discordUsername: { type: String, required: true },
-  socialProfiles: { type: [String], required: true },
-  web3Knowledge: { type: String, required: true },
-  interests: { type: [String], required: true },
-  onboardingInfo: { type: String, required: true },
-  taskCompletion: { type: [String], required: true },
-  progress: { type: String, required: true },
-  growth: { type: String, required: true },
-  internalKPIs: { type: String, required: true },
+  ethAddress: { type: String, required: true },
+  twitterHandle: { type: String, required: true },
+  discordHandle: { type: String, required: true },
+  interests: { type: String, required: true },
+  ambassadorExperience: { type: String, required: true },
+  termsAndConditions: { type: Boolean, required: true },
 }, {
   timestamps: true,
 });
