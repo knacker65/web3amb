2f8863f05ffb9dcba1a8ddc2d30adcda8d875693
knacker65 <67683942+knacker65@users.noreply.github.com>
2023-06-27 10:56:00 +0200

    Update project.model.js

--- backend/models/project.model.js
+++ backend/models/project.model.js
@@ -3,18 +3,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const projectSchema = new Schema({
-  name: { type: String, required: true },
+  projectName: { type: String, required: true },
+  projectEmail: { type: String, required: true },
+  projectWebsite: { type: String, required: true },
+  industry: { type: String, required: true },
   description: { type: String, required: true },
-  mission: { type: String, required: true },
-  keyInitiatives: { type: [String], required: true },
-  startDate: { type: Date, required: true },
-  endDate: { type: Date, required: true },
-  milestones: { type: [String], required: true },
-  programStructure: { type: String, required: true },
-  rewards: { type: [String], required: true },
-  rewardStructure: { type: String, required: true },
-  brandAssets: { type: [String], required: true },
-  onboardingTasklist: { type: [String], required: true },
+  blockchainPlatform: { type: String, required: true },
+  roles: { type: [String], required: true },
 }, {
   timestamps: true,
 });
