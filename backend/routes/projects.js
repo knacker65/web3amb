4b3dafa72475922fb25ae1d2118690f6b4658afc
knacker65 <67683942+knacker65@users.noreply.github.com>
2023-06-27 10:57:00 +0200

    Update projects.js

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -4,15 +4,24 @@ let Project = require('../models/project.model');
 router.route('/').get((req, res) => {
   Project.find()
     .then(projects => res.json(projects))
-    .catch(err => res.status(400).json('Error: ' + err));
+    .catch(err => {
+      console.error(err);
+      res.status(400).json('Error: ' + err);
+    });
 });
 
 router.route('/add').post((req, res) => {
   const newProject = new Project(req.body);
 
   newProject.save()
-    .then(() => res.json('Project added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
+    .then(() => {
+      console.log('Project added:', req.body);
+      res.json('Project added!');
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(400).json('Error: ' + err);
+    });
 });
 
 module.exports = router;
