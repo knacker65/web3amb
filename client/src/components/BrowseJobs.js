5101aa95d136cedf939d02775e35c422cf9d74ff
knacker65 <67683942+knacker65@users.noreply.github.com>
2023-06-27 10:58:38 +0200

    Update BrowseJobs.js

--- client/src/components/BrowseJobs.js
+++ client/src/components/BrowseJobs.js
@@ -3,11 +3,15 @@ import axios from 'axios';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import { useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoneyBill, faClock, faCode } from '@fortawesome/free-solid-svg-icons';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const BrowseJobs = () => {
   const jobsFromRedux = useSelector(state => state.jobs);
   const [searchTerm, setSearchTerm] = useState('');
   const [jobs, setJobs] = useState([]);
+  const [selectedJob, setSelectedJob] = useState(null);
 
   useEffect(() => {
     axios.get('http://localhost:5000/jobPostings')
@@ -16,14 +20,22 @@ const BrowseJobs = () => {
       })
       .catch(error => {
         console.log(error);
+        // Add your error handling code here
       });
   }, []);
-
-  const filteredJobs = jobs.filter(job =>
-    job.title && job.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
   
 
+  const filteredJobs = jobs.filter(job =>
+    job.jobTitle && job.jobTitle.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const handleViewDetails = (job) => {
+    setSelectedJob(job);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedJob(null);
+  };
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
@@ -34,7 +46,7 @@ const BrowseJobs = () => {
           <input
             type="text"
             className="w-full p-2 border border-gray-300 rounded"
-            placeholder="Search jobs..."
+            placeholder="Search jobs..."            
             value={searchTerm}
             onChange={event => setSearchTerm(event.target.value)}
           />
@@ -53,26 +65,56 @@ const BrowseJobs = () => {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-  {jobs.map((job) => (
-    <div
-      key={job._id}
-      className="border border-gray-200 p-4 rounded bg-white shadow"
+  {filteredJobs.map((job) => (
+    <motion.div key={job._id} className="border border-gray-200 p-4 rounded bg-white shadow"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
     >
       <h3 className="text-xl font-bold mb-2">{job.jobTitle}</h3>
       <p className="text-gray-500 mb-2">Company: {job.companyName}</p>
-      <p className="text-gray-500 mb-2">Location: {job.jobLocation}</p>
-      <p className="text-gray-500 mb-2">Type: {job.jobType}</p>
-      <p className="text-gray-500 mb-2">Description: {job.jobDescription}</p>
-      <button className="bg-blue-500 text-white px-4 py-2 mt-4 rounded">
-        Apply Now
-      </button>
-      <button className="bg-green-500 text-white px-4 py-2 mt-4 rounded ml-4">
+      <p className="text-gray-500 mb-2">Deadline: {new Date(job.deadline).toLocaleDateString()}</p>
+      <p className="text-gray-500 mb-2">Description: {job.jobDescription.substring(0, 80)}...</p>
+      <button className="bg-green-500 text-white px-4 py-2 mt-4 rounded ml-4" onClick={() => handleViewDetails(job)}>
         View Details
       </button>
-    </div>
+      </motion.div>
   ))}
 </div>
-
+{selectedJob && (
+          <motion.div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            exit={{ opacity: 0 }}
+            onClick={handleCloseModal} // Add this line
+          >
+            <motion.div className="bg-white p-8 rounded shadow-lg relative"
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              transition={{ duration: 0.5 }}
+              onClick={(e) => e.stopPropagation()}
+            >
+              <motion.div className="bg-white p-8 rounded shadow-lg relative overflow-auto max-h-screen"
+  initial={{ scale: 0 }}
+  animate={{ scale: 1 }}
+  transition={{ duration: 0.5 }}
+  onClick={(e) => e.stopPropagation()}
+></motion.div>
+              <button className="absolute top-0 right-0 p-4" onClick={handleCloseModal}>X</button>
+              <h2 className="text-xl font-bold mb-2">{selectedJob.jobTitle}</h2>
+              <p className="text-gray-500 mb-2">Company: {selectedJob.companyName}</p>
+              <p className="text-gray-500 mb-2">Location: {selectedJob.jobLocation}</p>
+              <p className="text-gray-500 mb-2">Type: {selectedJob.jobType}</p>
+              <p className="text-gray-500 mb-2">Description: {selectedJob.jobDescription}</p>
+              <p className="text-gray-500 mb-2">Reward Structure: {selectedJob.rewardStructure}</p>
+              <p className="text-gray-500 mb-2">Application Process: {selectedJob.applicationProcess}</p>
+              <p className="text-gray-500 mb-2">Job Requirements: {selectedJob.jobRequirements}</p>
+              <p className="text-gray-500 mb-2">Skills and Qualifications: {selectedJob.skillsAndQualifications}</p>
+              <p className="text-gray-500 mb-2">Deadline: {new Date(selectedJob.deadline).toLocaleDateString()}</p>
+            </motion.div>
+          </motion.div>
+        )}
       </div>
       <Footer />
     </div>
