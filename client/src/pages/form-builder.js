3fe848ccb0f880f40c7c30f3d29cfaf92a2adba6
knacker65 <67683942+knacker65@users.noreply.github.com>
2023-07-03 23:23:00 +0200

    Add files via upload

---
+++ client/src/pages/form-builder.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FormBuilder from '../components/FormBuilder';
+
+const FormBuilderPage = () => {
+  return (
+    <div>
+      <h1>Form Builder</h1>
+      <FormBuilder />
+    </div>
+  );
+};
+
+export default FormBuilderPage;
